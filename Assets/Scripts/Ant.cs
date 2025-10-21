387695cecc363cc3cbd1dc9210b53361cdccb690
meeteedee <meepoohfisho@gmail.com>
2025-10-21 16:10:53 +0700

    ant move v2

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -7,12 +7,31 @@ public class Ant : Enemy
 {
     
     [SerializeField] Vector2 velocity;
-    public Transform[] MovePoint;
+    public Transform[] movePoint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public override void Behavior()
     {
         rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        
+        if ( velocity.x < 0 && rb.position.x <= movePoint[0].position.x )
+        {
+            Flip();
+        }
+        //move right และเกินขอบขวา
+        if ( velocity.x > 0 && rb.position.x >= movePoint[1].position.x )
+        {
+            Flip();
+        }
+    }
+    
+    public void Flip()
+    {
+        velocity.x *= -1; //change direction of movement
+        //Flip the image
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
     void Start()
